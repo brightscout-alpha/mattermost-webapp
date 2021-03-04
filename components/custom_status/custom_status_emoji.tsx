b30fd126a91003564932f09622260fc954e9f36c
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2021-03-04 15:10:26 +0530

    Get custom emojis for custom status

--- components/custom_status/custom_status_emoji.tsx
+++ components/custom_status/custom_status_emoji.tsx
@@ -1,12 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Tooltip} from 'react-bootstrap';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+
+import {getCustomEmojis} from 'mattermost-redux/actions/emojis';
 
 import OverlayTrigger from 'components/overlay_trigger';
 import RenderEmoji from 'components/emoji/render_emoji';
 import {makeGetCustomStatus, isCustomStatusEnabled} from 'selectors/views/custom_status';
+import {isCustomEmojiEnabled} from 'selectors/emojis';
 import {GlobalState} from 'types/store';
 import Constants from 'utils/constants';
 
@@ -21,8 +24,17 @@ interface ComponentProps {
 }
 
 const CustomStatusEmoji = (props: ComponentProps) => {
+    const dispatch = useDispatch();
     const getCustomStatus = makeGetCustomStatus();
     const {emojiSize, emojiStyle, spanStyle, showTooltip, tooltipDirection, userID, onClick} = props;
+
+    const customEmojiEnabled = useSelector(isCustomEmojiEnabled);
+    useEffect(() => {
+        if (customEmojiEnabled) {
+            dispatch(getCustomEmojis());
+        }
+    }, [isCustomEmojiEnabled]);
+
     const customStatusEnabled = useSelector(isCustomStatusEnabled);
     const customStatus = useSelector((state: GlobalState) => {
         return getCustomStatus(state, userID);
